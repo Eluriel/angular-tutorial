09a0a206fe0da5d4fd65615ee27c7c9c430ee153
Benjamin Guex <benjamin.guex@asyril.com>
2025-06-24 16:38:48 +0200

    app/home: Add a filter on the city

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import {HousingLocationInfo} from '../housinglocation';
 import {HousingLocation} from '../housing-location/housing-location';
@@ -6,16 +7,16 @@ import {HousingService} from '../housing.service';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocation],
+  imports: [HousingLocation, FormsModule],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+      <form (keydown.enter)="filterResults(filter.value)">
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      @for (housingLocation of housingLocationList; track housingLocation.id){
+      @for (housingLocation of filteredLocationList; track housingLocation.id){
         <app-housing-location
         [housingLocation]="housingLocation"></app-housing-location>
       }
@@ -25,10 +26,22 @@ import {HousingService} from '../housing.service';
 })
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
+  filteredLocationList: HousingLocationInfo[] = [];
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 
 }
